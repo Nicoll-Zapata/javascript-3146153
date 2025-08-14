45f49086a8d47e15ce5be5fbf1e1d5580a63b513
Nicoll Zapata <zapatanicolmeliza@gmail.com>
2025-08-14 15:38:11 -0500

    Audio-video

---
+++ 4_audio_video_js/app.js
@@ -0,0 +1,7 @@
+const video = document.querySelector ('#video')
+const play = document.querySelector ('#play-video')
+const stopVideo = document.querySelector ('#stop-video')
+const playStop1 = document.querySelector ('#play-stop-img1')
+const playStop2 = document.querySelector ('#play-stop-img2')
+
+console.log(video,play,stopVideo,playStop1,playStop2)
